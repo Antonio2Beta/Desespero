b9390738eedac3ea72a74cf525c4b8b433215f35
Antonio2Beta <38213571+Antonio2Beta@users.noreply.github.com>
2018-12-25 14:47:30 -0300

    Update Operadores.cpp

--- Operadores.cpp
+++ Operadores.cpp
@@ -1,1 +1,30 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  int operador1, operador2, resultado;
+  char operando;
+  cout << "Digite uma expressão do tipo 'operando operador operando'(ex:4+2): ";
+  cin >> operador1 >> operando >> operador2;
+  switch(operando){
+    case '+':
+      resultado = operador1 + operador2;
+      break;
+    case '-':
+      resultado = operador1 - operador2;
+      break;
+    case '*':
+      resultado = operador1 * operador2;
+      break;
+    case '/':
+      resultado = operador1 / operador2;
+      break;
+    case '^':
+      resultado = pow(operador1, operador2);
+      break;
+    default:
+      cout << "Algo de errado não está certo, my consagrated =O" << endl;
+  }
+  cout << "O resultado da operação é: " << resultado << endl;
+  return 0;
+}
