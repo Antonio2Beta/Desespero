43912a21962935051462f6b22cfdd9506d1e92c7
Antonio2Beta <38213571+Antonio2Beta@users.noreply.github.com>
2019-01-10 13:09:33 -0300

    Create Tabuada.cpp

---
+++ Tabuada.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	for(int n1=1;n1<=10;n1++){
+		cout << "Tabuada do número " << n1 << endl;
+		for(int n2=1;n2<=10;n2++)
+		cout << n1 << "*" << n2 << "=" << n1*n2 << endl;
+	cout << endl;
+	}
+	return 0;
+}
